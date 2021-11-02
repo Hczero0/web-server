4d230c9d76e1b80198812e732861acf56e9a7108
Hczero0 <93322363+Hczero0@users.noreply.github.com>
2021-11-02 18:39:04 +0800

    Add files via upload

---
+++ Web server/03.面向对象开发web服务器.py
@@ -0,0 +1,81 @@
+import os.path
+import socket
+import threading
+
+
+class HttpWebserver(object):
+    def __init__(self):
+        # 创建TCP服务端的套接字
+        tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # =设置端口号复用
+        tcp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        # 绑定端口
+        tcp_server_socket.bind(("", 9000))
+        # 设置监听
+        tcp_server_socket.listen(128)
+        self.tcp_server_socket = tcp_server_socket
+
+    @staticmethod
+    def handle_client_request(new_socket):
+        recv_data = new_socket.recv(4096)
+
+        if len(recv_data) == 0:
+            new_socket.close()
+            return
+
+        # 对二进制进行解码
+        recv_content = recv_data.decode("utf-8")
+        print(recv_content)
+        request_list = recv_content.split(" ", maxsplit=2)
+        request_path = request_list[1]
+        print(request_path)
+
+        if request_path == "/":
+            request_path = "/index.html"
+        #   判断访问的数据在不在文件内
+        # 1.if os.path.exists("static/" + request_path):
+        # 2.try except
+        try:
+            # 打开文件中传送的网页数据
+            # window上需要加上encoding="utf-8" 以为OPEN在windows上内置编码是gbk
+            # 为了兼容图片打开 需要用rb
+            with open("static" + request_path, "rb") as file:
+                file_data = file.read()
+        #   没找到网页 返回404指令
+        except Exception as e:
+            response_line = "HTTP/1.1 404 NOT FOUNd\r\n"
+            # 相应头
+            response_header = "server: pws/1.0\r\n"
+            with open("static/error.html", "rb") as file:
+                file_data = file.read()
+            response_body = file_data
+            response = (response_line + response_header + "\r\n").encode("utf-8") + response_body
+            new_socket.send(response)
+        else:
+            # 相应行
+            response_line = "HTTP/1.1 200 OK\r\n"
+            # 相应头
+            response_header = "server: pws/1.0\r\n"
+            # 响应体
+            response_body = file_data
+
+            response = (response_line + response_header + "\r\n").encode("utf-8") + response_body
+            new_socket.send(response)
+        finally:
+            new_socket.close()
+
+    def start(self):
+        while True:
+            new_socket, ip_port = self.tcp_server_socket.accept()
+            sub_thread = threading.Thread(target=self.handle_client_request, args=(new_socket,))
+            sub_thread.setDaemon(True)
+            sub_thread.start()
+
+
+def main():
+    web_server = HttpWebserver()
+    web_server.start()
+
+
+if __name__ == '__main__':
+    main()
